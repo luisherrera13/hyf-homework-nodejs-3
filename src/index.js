5802d25bc3e50fcd13c67987d48fbc1083517cfa
luisherrera13 <jlhcorp2@hotmail.com>
2019-11-29 20:24:58 -0700

    the 2 steps of this test is getting passed in local repo branch (hello world and get/users response endpoint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ var express = require('express');
 
 var app = express();
 
-require('./routes.js')(app);
+var app = express();app.get('/', function (req, res) {
+    res.send('Hello World!');
+  });
+
+  require('./routes.js')(app);
 
 app.listen(3000);
 console.log("Jammin\' on port 3000...");
