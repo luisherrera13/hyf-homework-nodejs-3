df083b4f64b2e3b6ade0b608c246d19e63ce1f9c
luisherrera13 <jlhcorp2@hotmail.com>
2019-12-03 02:31:46 -0700

    last created user

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
   exports.findAll = function(req, res){
-  //var user = require('../controllers/sample_data.js')
-  var user = [];
+  var user = require('../controllers/sample_data.js')
+  //var user = [];
   res.send(user)
   }; //muestra un arreglo vacio pero si quiere mostrar el contenido del array descomentarlo y cometar el array vacio
