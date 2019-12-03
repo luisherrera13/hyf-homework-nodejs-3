62a1235528e6e1b91b88e28d3919092d91d2c915
luisherrera13 <jlhcorp2@hotmail.com>
2019-12-03 12:14:19 -0700

    all tests of step 3 passed in local testing

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,32 @@
 exports.add = function(req, res) {
-    var ids=0;
+    const fs = require('fs')
+    //var ids=0;
     //var ids=parseInt(req.params.id);
-    newID = { id: ids  };
-    res.send(newID);
+    //newID = { id: ids  };
+    //res.send(newID);
    // cuando se hizo la prueba solo se mando hacer un post en la url http://localhost:3000/user/ asi tal cual sin id  
-        };
+   
+   
+   var path = './controllers/sample_data.js';
+    fs.access(path, fs.F_OK, (err) => {
+      if (err) {
+        var user = [];
+        user.push('module.exports = [{ "id": 0 }];');
+        var json1 = user;
+        fs.writeFile('./controllers/sample_data.js', json1, function (err) {
+            if (err) throw err;
+        console.error(err)
+        console.log('Saved!');
+        user = require('../controllers/sample_data.js')
+        res.send(user);
+                                          }); 
+        //return
+      }else{
+      user = require('../controllers/sample_data.js')
+      res.send(user);}
+      //file exists
+                                            })
+   };
        
   //exports.add = function() {};
   
